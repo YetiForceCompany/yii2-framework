fa3a9d690b45842fb61f71d806dfbcf6285c3f8e
Mariusz Krzaczkowski <mariusz.krzaczkowski@gmail.com>
2018-03-24 22:17:48 +0100

    Added modifications to the engine YetiForceCRM

--- Yii.php
+++ Yii.php
@@ -6,7 +6,6 @@
  * @copyright Copyright (c) 2008 Yii Software LLC
  * @license http://www.yiiframework.com/license/
  */
-
 require(__DIR__ . '/BaseYii.php');
 
 /**
@@ -20,8 +19,48 @@ require(__DIR__ . '/BaseYii.php');
  */
 class Yii extends \yii\BaseYii
 {
+
+	public static $logToProfile;
+
+	/**
+	 * Marks the beginning of a code block for profiling.
+	 * This has to be matched with a call to [[endProfile]] with the same category name.
+	 * The begin- and end- calls must also be properly nested. For example,
+	 *
+	 * ```php
+	 * \Yii::beginProfile('block1');
+	 * // some code to be profiled
+	 *     \Yii::beginProfile('block2');
+	 *     // some other code to be profiled
+	 *     \Yii::endProfile('block2');
+	 * \Yii::endProfile('block1');
+	 * ```
+	 * @param string $token token for the code block
+	 * @param string $category the category of this log message
+	 * @see endProfile()
+	 */
+	public static function beginProfile($token, $category = 'application')
+	{
+		if (static::$logToProfile) {
+			self::getLogger()->log($token, \yii\log\Logger::LEVEL_PROFILE_BEGIN, $category);
+		}
+	}
+
+	/**
+	 * Marks the end of a code block for profiling.
+	 * This has to be matched with a previous call to [[beginProfile]] with the same category name.
+	 * @param string $token token for the code block
+	 * @param string $category the category of this log message
+	 * @see beginProfile()
+	 */
+	public static function endProfile($token, $category = 'application')
+	{
+		if (static::$logToProfile) {
+			self::getLogger()->log($token, \yii\log\Logger::LEVEL_PROFILE_END, $category);
+		}
+	}
 }
 
 spl_autoload_register(['Yii', 'autoload'], true, true);
-Yii::$classMap = require(__DIR__ . '/classes.php');
+Yii::$classMap = require(__DIR__ . '/classesYetiForce.php');
 Yii::$container = new yii\di\Container();
